202f9a18ef0360c143d619f315ada173245a68fb
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-19 19:03:07 +0200

    Fixed App Routing ft app.html <router-outlet>
    deleted auto data, added new inside app.comp.html with router outlet to display changes and components

--- my-ufcfightsite-app/src/app/app-routing.module.ts
+++ my-ufcfightsite-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventComponent } from './components/event/event.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { FightersComponent } from './components/fighters/fighters.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'feature', component: FeatureComponent },
+  { path: 'event/:id', component: EventComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: 'fighters', component: FightersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
