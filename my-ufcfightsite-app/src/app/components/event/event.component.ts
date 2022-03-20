4d6bc0260a7a957b6e9bf6a7d4e06ea60a746fea
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-20 17:29:52 +0200

    UPDATED SCHEDULE EVENT

--- my-ufcfightsite-app/src/app/components/event/event.component.ts
+++ my-ufcfightsite-app/src/app/components/event/event.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+// @Component({
+//   selector: 'app-event',
+//   templateUrl: './event.component.html',
+//   styleUrls: ['./event.component.css']
+// })
+// export class EventComponent implements OnInit {
+//   clickedEvent;
+//   constructor() { }
+
+//   ngOnInit(): void {
+
+//   }
+// }
+
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.scss']
 })
+
 export class EventComponent implements OnInit {
+<<<<<<< Updated upstream
 
   constructor() { }
 
   ngOnInit(): void {
+=======
+  clickedEvent;
+  //super duper weird ding want mense laat grys word
+
+  constructor(public apiService: ApiService, public route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    let eventId = this.route.snapshot.paramMap.get('id');
+    this.apiService.getEvent(eventId).then(event => {
+      this.clickedEvent = event;
+      console.log(this.clickedEvent);
+    })
+>>>>>>> Stashed changes
   }
 
 }
