ffd4d51c46351471cfe21bc0369a02e57dcb5131
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-20 22:52:18 +0200

    FIX: readded api service

--- my-ufcfightsite-app/src/app/services/api.service.ts
+++ my-ufcfightsite-app/src/app/services/api.service.ts
@@ -11,17 +11,24 @@ export class ApiService {
   }
 
   async getEvent(eventId: string) {
-    return await fetch(`https://us-central1-fight-site-6dbcd.cloudfunctions.net/getEvent`)
+    const payload = { "eventId" : eventId }
+
+    return await fetch(`https://us-central1-fight-site-6dbcd.cloudfunctions.net/getEvent`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
       .then(data => data.json())
   }
 
   async getFight(fightId: string) {
-    return await fetch(`https://api.sportsdata.io/v3/mma/stats/json/Fight/${fightId}?key=0d7764cec6ff4982b6257b85ab293701`)
+    return await fetch(`https://us-central1-fight-site-6dbcd.cloudfunctions.net/getFight`)
       .then(data => data.json())
   }
 
-  async getFighter(fighterId: string) {
-    return await fetch(`https://api.sportsdata.io/v3/mma/scores/json/Fighter/${fighterId}?key=0d7764cec6ff4982b6257b85ab293701`)
+  async getFighters() {
+    return await fetch(`https://us-central1-fight-site-6dbcd.cloudfunctions.net/getFighters`)
       .then(data => data.json())
   }
 }
