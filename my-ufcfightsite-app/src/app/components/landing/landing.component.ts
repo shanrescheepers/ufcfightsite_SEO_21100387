6a936049b03f492cc42d1561ba62cedc9c8f71be
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-21 01:34:23 +0200

    added landing page event

--- my-ufcfightsite-app/src/app/components/landing/landing.component.ts
+++ my-ufcfightsite-app/src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAnalytics } from '@angular/fire/compat/analytics';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public analytics: AngularFireAnalytics) { }
 
   ngOnInit(): void {
+    this.analytics.logEvent("landing_page_viewed");
   }
 
 }
