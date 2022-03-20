65440a0c2750ecf9609f702a3b5bf050b519d781
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-20 21:44:15 +0200

    FIX:  API working again

--- my-ufcfightsite-app/src/app/services/api.service.ts
+++ my-ufcfightsite-app/src/app/services/api.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   async getSchedule() {
-    return await fetch("https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/2022?key=0d7764cec6ff4982b6257b85ab293701")
+    return await fetch("https://us-central1-fight-site-6dbcd.cloudfunctions.net/getSchedule")
       .then(data => data.json())
   }
 
   async getEvent(eventId: string) {
-    return await fetch(`https://api.sportsdata.io/v3/mma/scores/json/EVENT/${eventId}?key=0d7764cec6ff4982b6257b85ab293701`)
+    return await fetch(`https://us-central1-fight-site-6dbcd.cloudfunctions.net/getEvent`)
       .then(data => data.json())
   }
 
