263de92b80cf2757d79c35462c0454c1560c0976
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-23 22:23:10 +0200

    FIX: error->console.log
    Taken out CORS pride.... but I still win... next time cors, next time (please no)

--- my-ufcfightsite-app/functions/index.js
+++ my-ufcfightsite-app/functions/index.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const functions = require("firebase-functions");
 const axios = require('axios');
-//firebase is serverless, ons kan n backend gee met hierdie JS wat geskryf word. Hierdie is basies die "backend"
+//firebase is serverless, kan n backend gee met hierdie JS wat geskryf word. Hierdie is basies die "backend"
 
 //cors resolved schedule 
 exports.getSchedule = functions.https.onRequest(async (req, res) => {
@@ -17,19 +17,19 @@ exports.getSchedule = functions.https.onRequest(async (req, res) => {
             return res.status(200).send({ data: data })
         }
     } catch (error) {
-        alert(error)
+        console.log(error)
     }
 });
 // get event
 exports.getEvent = functions.https.onRequest(async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*')
     res.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT')
-    // fuck u cors
+
     // https://stackoverflow.com/questions/62818364/firebase-cloud-function-cors-error-with-axios-request
     res.set('Access-Control-Allow-Headers', 'Content-Type')
 
     key = '0d7764cec6ff4982b6257b85ab293701'
-    eventId = req.body; //set req(soos promise), ons soek body van request. event id na hierdie om hier onder te gebruik
+    eventId = req.body; //set req(soos die promise), soek body van die request. dan event id na hierdie om hier onder te gebruik
     try {
         const response = await axios.get(`https://api.sportsdata.io/v3/mma/scores/json/EVENT/${eventId}?key=${key}`)
         const data = await response.data
